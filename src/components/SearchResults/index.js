965d66fe85a059c458f30ea79735a43703ec0ac4
Mike Flynn <mdflynn34@outlook.com>
2021-06-12 15:35:45 -0600

    add bootstrap for result card

--- src/components/SearchResults/index.js
+++ src/components/SearchResults/index.js
@@ -1,9 +1,31 @@
-import React from "react"
+import React from "react";
+import "./SearchResults.css";
+// Bootstrap Components
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
 
-const SearchResults = () => {
-    return (
-        <h1>Search Results</h1>
-    )
-}
+const SearchResults = ({ data }) => {
+  const createResultCards = () => {
+    return data.map((result) => {
+      return (
+        <div className="row">
+          <div className="col-sm-6">
+            <Card style={{ width: "18rem" }} key={result.id}>
+              <Card.Img variant="top" src={result.owner.avatar_url} />
+              <Card.Body>
+                <Card.Title>{result.name}</Card.Title>
+                <Card.Text>Language: {result.language}</Card.Text>
+                <Card.Text>Total ⭐'s: {result.stargazers_count}</Card.Text>
+                <Button variant="primary">View Details</Button>
+              </Card.Body>
+            </Card>
+          </div>
+        </div>
+      );
+    });
+  };
+
+  return <div className="card-deck">{createResultCards()}</div>;
+};
 
 export default SearchResults;
