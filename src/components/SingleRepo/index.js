597c9053fbe9f05688262c4d058063fd3e806f61
Mike Flynn <mdflynn34@outlook.com>
2021-06-12 17:16:06 -0600

    add singleRepo bootstrap details

--- src/components/SingleRepo/index.js
+++ src/components/SingleRepo/index.js
@@ -1,9 +1,28 @@
-import React from "react"
+import React from "react";
+import "./SingleRepo.css";
+// Bootstrap Components
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
 
-const SingleRepo = () => {
-    return (
-        <h1>Single Repo</h1>
-    )
-}
+const SingleRepo = ({ data }) => {
+  return (
+    <Card style={{ width: "18rem" }}>
+      <Card.Img variant="top" src={data.owner.avatar_url} />
+      <Card.Body>
+        <Card.Title>{data.name}</Card.Title>
+        <Card.Text>Language: {data.language}</Card.Text>
+        <Card.Text>Total ⭐'s: {data.stargazers_count}</Card.Text>
+        <Button
+          variant="primary"
+          target="_blank"
+          rel="noreferrer"
+          href={data.html_url}
+        >
+          View Repo
+        </Button>
+      </Card.Body>
+    </Card>
+  );
+};
 
 export default SingleRepo;
