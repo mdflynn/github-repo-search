7eca58fbc379dd4a760f575b7cbd4d491e524e85
Mike Flynn <mdflynn34@outlook.com>
2021-06-12 10:52:22 -0600

    add additional styling to searchform

--- src/components/SearchForm/index.js
+++ src/components/SearchForm/index.js
@@ -2,19 +2,27 @@ import React from "react";
 // Bootstrap Components
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
+import Col from "react-bootstrap/Col";
 
 const SearchForm = () => {
   return (
     <Form>
       <Form.Group controlId="formSearch">
         <Form.Label>Search repos on GitHub!</Form.Label>
-        <Form.Control type="text" placeholder="Enter search criteria" />
+        <Form.Row className="align-items-center">
+          <Col xs="6">
+            <Form.Control type="text" placeholder="Enter search criteria" />
+          </Col>
+          <Col xs="auto">
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Col>
+        </Form.Row>
+
         <Form.Text className="text-muted">
           Try a username, project title, or a topic!
         </Form.Text>
-        <Button variant="primary" type="submit">
-          Submit
-        </Button>
       </Form.Group>
     </Form>
   );
