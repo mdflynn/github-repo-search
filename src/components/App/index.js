53b2e1e338333b1ef202f0c91a1e148f0a6374f6
Mike Flynn <mdflynn34@outlook.com>
2021-06-12 14:04:19 -0600

    create api func to pass as prop

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 // Components
 import SearchForm from "../SearchForm";
@@ -6,14 +6,23 @@ import SearchResults from "../SearchResults";
 import SingleRepo from "../SingleRepo";
 import MainPage from "../MainPage";
 // API
-import { searchRepo } from "../../apiCalls";
+import { searchAllRepos } from "../../apiCalls";
 // Router
 import { Route, Switch } from "react-router-dom";
 
 function App() {
+  const [searchResults, setSearchResults] = useState([]);
+  const [repoView, setRepoView] = useState({});
+
+  const searchRepos = (searchCriteria) => {
+    searchAllRepos(searchCriteria)
+      .then((res) => res.json())
+      .then((data) => setSearchResults(data.items));
+  };
+
   return (
     <>
-      <SearchForm />
+      <SearchForm api={searchRepos} />
       <Switch>
         <Route exact path="/" component={MainPage} />
         <Route path="/search-results" component={SearchResults} />
