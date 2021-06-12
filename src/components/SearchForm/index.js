ae7e1523a6491a264751f6623a54b8d22eb95312
Mike Flynn <mdflynn34@outlook.com>
2021-06-12 10:38:24 -0600

    Add form bootstrap structure

--- src/components/SearchForm/index.js
+++ src/components/SearchForm/index.js
@@ -1,9 +1,23 @@
 import React from "react";
+// Bootstrap Components
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 
 const SearchForm = () => {
-    return (
-        <h1>Search Form</h1>
-    )
-}
+  return (
+    <Form>
+      <Form.Group controlId="formSearch">
+        <Form.Label>Search repos on GitHub!</Form.Label>
+        <Form.Control type="text" placeholder="Enter search criteria" />
+        <Form.Text className="text-muted">
+          Try a username, project title, or a topic!
+        </Form.Text>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form.Group>
+    </Form>
+  );
+};
 
 export default SearchForm;
