f355df48dcea51008a80c317144dc7f5c1f846fb
Mike Flynn <mdflynn34@outlook.com>
2021-06-12 16:03:50 -0600

    add singlerepo view function

--- src/components/SearchResults/index.js
+++ src/components/SearchResults/index.js
@@ -4,19 +4,28 @@ import "./SearchResults.css";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 
-const SearchResults = ({ data }) => {
+const SearchResults = ({ data, api }) => {
+
+  const viewRepoDetails = (result) => {
+    const searchCriteria = `${result.owner.login}/${result.name}`
+    api(searchCriteria);
+  };
+
+  // prop function to pass detail info up
   const createResultCards = () => {
     return data.map((result) => {
       return (
-        <div className="row">
+        <div className="row" key={result.id}>
           <div className="col-sm-6">
-            <Card style={{ width: "18rem" }} key={result.id}>
+            <Card style={{ width: "18rem" }} >
               <Card.Img variant="top" src={result.owner.avatar_url} />
               <Card.Body>
                 <Card.Title>{result.name}</Card.Title>
                 <Card.Text>Language: {result.language}</Card.Text>
                 <Card.Text>Total ⭐'s: {result.stargazers_count}</Card.Text>
-                <Button variant="primary">View Details</Button>
+                <Button variant="primary" onClick={() => viewRepoDetails(result)}>
+                  View Details
+                </Button>
               </Card.Body>
             </Card>
           </div>
