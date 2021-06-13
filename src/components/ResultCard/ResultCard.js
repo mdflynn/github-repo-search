2a7211e42dd784a422467394d2a37939c836d465
Mike Flynn <mdflynn34@outlook.com>
2021-06-13 12:03:20 -0600

    create seperate component

---
+++ src/components/ResultCard/ResultCard.js
@@ -0,0 +1,28 @@
+import React from "react";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+
+const ResultCard = ({data, viewRepo}) => {
+    return (
+            <div className="row" key={data.id}>
+              <div className="col-sm-6">
+                <Card style={{ width: "18rem" }}>
+                  <Card.Body>
+                    <Card.Title>{data.name}</Card.Title>
+                    <Card.Text>Language: {data.language}</Card.Text>
+                    <Card.Text>Total ⭐'s: {data.stargazers_count}</Card.Text>
+                    <Button
+                      variant="primary"
+                      onClick={() => viewRepo(data)}
+                    >
+                      View Details
+                    </Button>
+                  </Card.Body>
+                </Card>
+              </div>
+            </div>
+          
+    )
+}
+
+export default ResultCard;
--- src/components/SearchResults/index.js
+++ src/components/SearchResults/index.js
@@ -1,8 +1,7 @@
 import React, { useEffect, useState } from "react";
 import "./SearchResults.css";
+import ResultCard from "../ResultCard/ResultCard";
 // Bootstrap Components
-import Button from "react-bootstrap/Button";
-import Card from "react-bootstrap/Card";
 import Pagination from "react-bootstrap/Pagination";
 import Dropdown from "react-bootstrap/Dropdown";
 
@@ -16,30 +15,9 @@ const SearchResults = ({ data, api, url }) => {
     api(searchCriteria);
   };
 
-  // prop function to pass detail info up
-
-  // seperate into component
   const createResultCards = () => {
     return data.map((result) => {
-      return (
-        <div className="row" key={result.id}>
-          <div className="col-sm-6">
-            <Card style={{ width: "18rem" }}>
-              <Card.Body>
-                <Card.Title>{result.name}</Card.Title>
-                <Card.Text>Language: {result.language}</Card.Text>
-                <Card.Text>Total ⭐'s: {result.stargazers_count}</Card.Text>
-                <Button
-                  variant="primary"
-                  onClick={() => viewRepoDetails(result)}
-                >
-                  View Details
-                </Button>
-              </Card.Body>
-            </Card>
-          </div>
-        </div>
-      );
+      return <ResultCard data={result} viewRepo={viewRepoDetails} />;
     });
   };
 
