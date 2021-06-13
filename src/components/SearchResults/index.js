ed540a14a734bb218ba081e310cc5675f9352241
Mike Flynn <mdflynn34@outlook.com>
2021-06-13 11:57:54 -0600

    add sort functionality to api query

--- src/components/SearchResults/index.js
+++ src/components/SearchResults/index.js
@@ -9,6 +9,7 @@ import Dropdown from "react-bootstrap/Dropdown";
 const SearchResults = ({ data, api, url }) => {
   const [pageNumber, setPageNumber] = useState(1);
   const [dropdownLang, setDropdownLang] = useState([]);
+  const [filteredUrl, setFilteredUrl] = useState("");
 
   const viewRepoDetails = (result) => {
     const searchCriteria = `${result.owner.login}/${result.name}`;
@@ -53,7 +54,7 @@ const SearchResults = ({ data, api, url }) => {
         <Dropdown.Item
           key={index}
           value={language}
-          onClick={(e) => apiFilterByLanguage(e)}
+          onClick={(e) => sortAndFilterResults(e)}
         >
           {language}
         </Dropdown.Item>
@@ -61,51 +62,83 @@ const SearchResults = ({ data, api, url }) => {
     });
   };
 
-  const apiFilterByLanguage = (e) => {
-    const searchUrl = `${url}+language:${e.target.getAttribute("value")}`;
+  const sortAndFilterResults = (e) => {
+    // Queries api with selected filter or sort options
+    const selectedValue = e.target.getAttribute("value");
+
+    let searchUrl;
+
+    switch (selectedValue) {
+      case "best-match":
+        searchUrl = url;
+        break;
+      case "stars":
+        searchUrl = `${url}&sort=stars&order=desc`;
+        break;
+      default:
+        searchUrl = `${url}+language:${e.target.getAttribute("value")}`;
+    }
+
+    setFilteredUrl(searchUrl);
     api(searchUrl);
   };
 
-  //   useEffect(() => {
-  //     const updatePage = `${search}&page=${pageNumber}`
-  //     api(updatePage)
-  //   }, [pageNumber])
+  useEffect(() => {
+    // Handles next page based on filtered results
+    const nextPageUrl = filteredUrl ? filteredUrl : url;
+    const updatePage = `${nextPageUrl}&page=${pageNumber}`;
+    api(updatePage);
+  }, [pageNumber]);
 
   const viewNextPage = () => {
     setPageNumber((pageNumber) => (pageNumber += 1));
-    // call api w/ updated page
-    // console.log(pageNumber);
-    // const updatePage = `${search}&page=${pageNumber}`
-    // api(updatePage)
   };
 
   const viewPreviousPage = () => {
     if (pageNumber > 1) {
       setPageNumber((pageNumber) => (pageNumber -= 1));
-      // call api w/ updated page
     }
   };
 
-  // use page number to search next page on api
-
   return (
-    <div>
-      <Dropdown>
-        <Dropdown.Toggle variant="success" id="dropdown-basic">
-          Filter by Language
-        </Dropdown.Toggle>
+    <>
+      <div className="button-container">
+        <Dropdown>
+          <Dropdown.Toggle variant="info" id="dropdown-basic">
+            Filter by Language
+          </Dropdown.Toggle>
+          <Dropdown.Menu>{generateDropdownItems(data)}</Dropdown.Menu>
+        </Dropdown>
 
-        <Dropdown.Menu>
-          {generateDropdownItems(data)}
-        </Dropdown.Menu>
-      </Dropdown>
+        <Dropdown>
+          <Dropdown.Toggle variant="info" id="dropdown-basic">
+            Sort Results
+          </Dropdown.Toggle>
+          <Dropdown.Menu>
+            <Dropdown.Item
+              value={"best-match"}
+              onClick={(e) => sortAndFilterResults(e)}
+            >
+              Best Match
+            </Dropdown.Item>
+            <Dropdown.Item
+              value={"stars"}
+              onClick={(e) => sortAndFilterResults(e)}
+            >
+              Star Rating
+            </Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+      </div>
+
       <div className="card-deck">{createResultCards()}</div>
+
       <Pagination>
         <Pagination.Prev onClick={viewPreviousPage} />
         <Pagination.Item>{pageNumber}</Pagination.Item>
         <Pagination.Next onClick={viewNextPage} />
       </Pagination>
-    </div>
+    </>
   );
 };
 
