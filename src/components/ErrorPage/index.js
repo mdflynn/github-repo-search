9947ee146d87f2cc4cf9896a869c8b33afe5b871
Mike Flynn <mdflynn34@outlook.com>
2021-06-13 16:04:15 -0600

    add error handling

---
+++ src/components/ErrorPage/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+
+const ErrorPage = ({ statusCode }) => {
+  return (
+    <Card style={{ width: "20rem" }}>
+      <Card.Img variant="top" src={`https://http.cat/${statusCode}`} />
+      <Card.Body>
+        <Card.Title>Error: {statusCode}</Card.Title>
+        <Card.Text>
+          Please Try Again
+        </Card.Text>
+      </Card.Body>
+    </Card>
+  );
+};
+
+export default ErrorPage;
