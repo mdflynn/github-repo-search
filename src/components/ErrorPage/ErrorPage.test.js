954246fc144ebf506f73fd65db397b82801daff3
Mike Flynn <mdflynn34@outlook.com>
2021-06-13 19:50:38 -0600

    seperate tests

--- src/components/ErrorPage/ErrorPage.test.js
+++ src/components/ErrorPage/ErrorPage.test.js
@@ -5,18 +5,23 @@ import { screen, render } from "@testing-library/react";
 import "@testing-library/jest-dom";
 
 describe("<ErrorPage />", () => {
-  //unit test
-
-  it("should render the ErrorPage component", () => {
-      
+  beforeEach(() => {
     render(<ErrorPage statusCode={404} />);
+  });
 
+  //unit test
+  it("should render an error image", () => {
     const errorImg = screen.getByRole("img");
-    const errorCode = screen.getByText("Error: 404");
-    const tryAgain = screen.getByText("Please Try Again");
-
     expect(errorImg).toBeInTheDocument();
+  });
+
+  it("should render the error status code", () => {
+    const errorCode = screen.getByText("Error: 404");
     expect(errorCode).toBeInTheDocument();
+  });
+
+  it("should render try again", () => {
+    const tryAgain = screen.getByText("Please Try Again");
     expect(tryAgain).toBeInTheDocument();
   });
 });
