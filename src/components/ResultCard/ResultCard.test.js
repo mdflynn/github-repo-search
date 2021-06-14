24bae40f5e99c3c407013bd3cab79f88c02f81b8
Mike Flynn <mdflynn34@outlook.com>
2021-06-13 19:50:46 -0600

    add unit tests

--- src/components/ResultCard/ResultCard.test.js
+++ src/components/ResultCard/ResultCard.test.js
@@ -3,10 +3,10 @@ import ResultCard from ".";
 // Testing Dependencies
 import { screen, render } from "@testing-library/react";
 import "@testing-library/jest-dom";
+import userEvent from "@testing-library/user-event";
 
 describe("<ResultCard />", () => {
-  //unit tests
-  it("should render a ResultCard", () => {
+  beforeEach(() => {
     const mockedResultData = {
       id: 123,
       name: "Sweet Repo",
@@ -17,13 +17,26 @@ describe("<ResultCard />", () => {
     const mockedFunction = jest.mock();
 
     render(<ResultCard data={mockedResultData} viewRepo={mockedFunction} />);
+  });
 
-    const repoName = screen.getByText('Sweet Repo')
-    const repoLang = screen.getByText('Language: JavaScript')
-    const repoStar = screen.getByText('Total ⭐\'s: 42')
-
+  //unit tests
+  it("should have a name", () => {
+    const repoName = screen.getByText("Sweet Repo");
     expect(repoName).toBeInTheDocument();
+  });
+
+  it("should have a language", () => {
+    const repoLang = screen.getByText("Language: JavaScript");
     expect(repoLang).toBeInTheDocument();
+  });
+
+  it("should have a star count", () => {
+    const repoStar = screen.getByText("Total ⭐'s: 42");
     expect(repoStar).toBeInTheDocument();
+  });
+
+  it("should have a view details button", () => {
+    const detailButton = screen.getByText("View Details");
+    expect(detailButton).toBeInTheDocument();
   });
 });
